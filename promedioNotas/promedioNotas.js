867cad7b0e5a59b3c2cb33fdb90adddc0555ce62
jonathanzm8 <jzhina.done@gmail.com>
2024-01-26 13:11:49 -0500

    reto 32 evidencia promedio version2

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -7,19 +7,39 @@ calcularPromedioNotas=function(){
     promedio= calcularPromedio(nota1,nota2,nota3);
 
     promedioRedondeado= promedio.toFixed(2);
+    console.log(promedioRedondeado)
 
-    cambiarTexto("lblResultado",promedioRedondeado);
+    // cambiarTexto("lblResultado",promedioRedondeado);
+    // esto del ejecion anterios de notas de promedio-----------------
+    // if(promedioRedondeado>7){
+    //     cambiarImagen("lblImg","./imagenes/feliz.gif")
 
-    if(promedioRedondeado>7){
-        cambiarImagen("lblImg","./imagenes/feliz.gif")
+    // }else{
+    //     cambiarImagen("lblImg","./imagenes/triste.gif")
 
-    }else{
-        cambiarImagen("lblImg","./imagenes/triste.gif")
+    // }
 
-    }
+        if(promedioRedondeado<5 && promedioRedondeado>0){
+            cambiarTexto("lblResultado","REPROBADO")
+            cambiarImagen("lblImg","./imagenes/triste.gif")
+    
+            }else if(promedioRedondeado>=5 && promedioRedondeado<=8){
+                cambiarTexto("lblResultado","BUENTRABAJO")
+                cambiarImagen("lblImg","./imagenes/buenTrabajo.gif")
+    
 
-   
+            }else if(promedioRedondeado>8 && promedioRedondeado<=10){
+                cambiarTexto("lblResultado","EXCELENTE")
+                cambiarImagen("lblImg","./imagenes/excelente.gif")
+                 
 
+            }else{
+
+                cambiarTexto("lblResultado","DATOS INCORRECTOS")
+                cambiarImagen("lblImg","./imagenes/error.gif")
+
+            }
+            
 
 
 
